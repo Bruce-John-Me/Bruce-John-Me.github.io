cc0c54f5094a5fefdc3b233d475a1a536ba69980
BJareval <141091726+BJareval@users.noreply.github.com>
2026-08-07 15:04:51 +0800

    HTML to one file and CSS

--- Personal Portfolio Design/src/App.tsx
+++ Personal Portfolio Design/src/App.tsx
@@ -2,7 +2,7 @@ import { useEffect } from 'react'
 
 export default function App() {
   useEffect(() => {
-    window.location.replace('/portfolio.html')
+    window.location.replace('/index.html')
   }, [])
   return null
 }
